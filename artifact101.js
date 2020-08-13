949c77566eb157dc7b288b0107bedaed696364e8
Phalakorn223 <69285696+Phalakorn223@users.noreply.github.com>
2020-08-13 11:32:58 +0700

    Add artifact101, artifact102

---
+++ artifact101.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+function App(){
+  return(
+    <View style={{
+      justifyContent:"center",
+      flex:1,
+      alignItems:"center"
+      }}>
+      <Text style={{color:"red",fontSize:"20px",fontWeight:"bold"}}>ไอ้เพชรรร มึงมันโง่ดีว่ะ</Text>
+    </View>
+  )
+}
+export default App;
